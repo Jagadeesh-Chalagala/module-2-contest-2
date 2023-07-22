fcc07524084cec4516cc802d370b9b28248127ce
Jagadeesh-Chalagala <117630691+Jagadeesh-Chalagala@users.noreply.github.com>
2023-07-22 23:15:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,170 @@
+// const data = [
+//   {
+//     name: "Veggie Delight",
+//     imageSrc: "https://source.unsplash.com/random?veggies",
+//     time: "30 min",
+//     type: "veg",
+//     isLiked: false,
+//     rating: 4.2,
+//   },
+//   {
+//     name: "Chicken Grill",
+//     imageSrc: "https://source.unsplash.com/random?chicken",
+//     time: "45 min",
+//     type: "non-veg",
+//     isLiked: false,
+//     rating: 4.5,
+//   },
+//   {
+//     name: "Cheese Pizza",
+//     imageSrc: "https://source.unsplash.com/random?pizza",
+//     time: "40 min",
+//     type: "veg",
+//     isLiked: false,
+//     rating: 4.1,
+//   },
+//   {
+//     name: "Steak",
+//     imageSrc: "https://source.unsplash.com/random?steak",
+//     time: "60 min",
+//     type: "non-veg",
+//     isLiked: false,
+//     rating: 4.7,
+//   },
+//   {
+//     name: "Grilled Salmon",
+//     imageSrc: "https://source.unsplash.com/random?salmon",
+//     time: "50 min",
+//     type: "non-veg",
+//     isLiked: false,
+//     rating: 4.6,
+//   },
+//   {
+//     name: "Tomato Pasta",
+//     imageSrc: "https://source.unsplash.com/random?pasta",
+//     time: "35 min",
+//     type: "veg",
+//     isLiked: false,
+//     rating: 4.0,
+//   },
+//   {
+//     name: "Vegan Salad",
+//     imageSrc: "https://source.unsplash.com/random?salad",
+//     time: "20 min",
+//     type: "veg",
+//     isLiked: false,
+//     rating: 3.9,
+//   },
+//   {
+//     name: "Fried Chicken",
+//     imageSrc: "https://source.unsplash.com/random?friedChicken",
+//     time: "55 min",
+//     type: "non-veg",
+//     isLiked: false,
+//     rating: 4.3,
+//   },
+//   {
+//     name: "Mushroom Risotto",
+//     imageSrc: "https://source.unsplash.com/random?risotto",
+//     time: "45 min",
+//     type: "veg",
+//     isLiked: false,
+//     rating: 4.5,
+//   },
+//   {
+//     name: "Burger",
+//     imageSrc: "https://source.unsplash.com/random?burger",
+//     time: "30 min",
+//     type: "non-veg",
+//     isLiked: false,
+//     rating: 4.2,
+//   },
+//   {
+//     name: "Paneer Tikka",
+//     imageSrc: "https://source.unsplash.com/random?paneerTikka",
+//     time: "40 min",
+//     type: "veg",
+//     isLiked: false,
+//     rating: 4.4,
+//   },
+//   {
+//     name: "BBQ Ribs",
+//     imageSrc: "https://source.unsplash.com/random?ribs",
+//     time: "70 min",
+//     type: "non-veg",
+//     isLiked: false,
+//     rating: 4.6,
+//   },
+//   {
+//     name: "Caesar Salad",
+//     imageSrc: "https://source.unsplash.com/random?caesarSalad",
+//     time: "25 min",
+//     type: "veg",
+//     isLiked: false,
+//     rating: 3.8,
+//   },
+//   {
+//     name: "Fish Tacos",
+//     imageSrc: "https://source.unsplash.com/random?fishTacos",
+//     time: "35 min",
+//     type: "non-veg",
+//     isLiked: false,
+//     rating: 4.3,
+//   },
+//   {
+//     name: "Chocolate Cake",
+//     imageSrc: "https://source.unsplash.com/random?chocolateCake",
+//     time: "90 min",
+//     type: "veg",
+//     isLiked: false,
+//     rating: 4.9,
+//   },
+// ];
+
+// // let menu = document.getElementsByClassName("menu-items")
+// // let card = document.createElement('div');
+// // card.style.width= "18rem";
+// // let image = document.createElement
+
+// let card = `
+
+// <div class="card" style="width: 18rem;">
+//                     <div class="image">
+//                         <img class="card-img-top" src="./images/cardImg1.png" alt="Card image cap">
+//                     </div>
+//                     <div class="card-body">
+//                       <div class="card-text">
+//                         <h5 style="font-size: medium;">Veg</h5>
+//                         <div class="name-container d-flex align-items-center justify-content-between">
+//                             <div>
+//                                 <h4 style="margin-top: 5px;" class="font-weight-bold">Russian Salard</h4>
+//                             </div>
+                            
+//                             <div class="rating d-flex align-items-center">
+//                                 <div class="icon mx-2">
+//                                     <i class="bi bi-star-fill text-warning"></i>
+//                                 </div>
+//                                 <div  >
+//                                     <p style="margin-top: 15px;">4.5</p>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                       </div>
+//                       <div class="time-container d-flex justify-content-between align-items-center">
+//                         <h4 class="text-danger ">40 min</h4>
+//                         <div class="icons d-flex justify-content-center align-items-center">
+//                             <div class="mx-2">
+//                                 <i class="bi bi-heart text-danger"></i>
+//                             </div>
+//                             <div>
+//                                 <i class="bi bi-chat"></i>
+//                             </div>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </div>`;
+
+let items = document.getElementsByClassName('menu-items')
+console.log(items);
+// events 
+
